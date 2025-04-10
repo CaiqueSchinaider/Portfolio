3dcf677c31da06344b0525563d5664e46e82f9d0
Caique Schinaider <caiqueschinaiderrufinoviana@gmail.com>
2025-04-10 13:06:02 -0300

    fix all

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,11 +10,19 @@ function Footer() {
       </div>
       <div className={styles.TelephoneNumberContainer}>
         <img
-          src="telephone.png"
+          src=" /contact/telephone.png"
           alt="telephone"
           className={styles.IconTelephone}
         />{" "}
         <p>55+ (31) 99152-2505</p>
+      </div>
+      <div className={styles.AddressContainer}>
+        <img
+          src="/contact/address.png"
+          alt="telephone"
+          className={styles.IconTelephone}
+        />{" "}
+        <p>Bonsucesso, Belo Horizonte | Minas Gerais - Brasil</p>
       </div>
     </section>
   );
